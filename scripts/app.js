6901684902986bfd01b231576626549c56ccacd2
Alberto <cerrone.alberto93@gmail.com>
2020-11-19 18:46:53 +0100

    Created different classes to handle different levels

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,31 @@
-function init() {
-  
+// TODO Functions to:
+// TODO handle 3 levels of gaming: -Beginner 9*9  -Intermediate 16*16 -Expert 30*16
 
+//* When game is starting, all the cells have one class of covered
+//*The 
+
+function init() {
+  //* Variables
+
+  const grid = document.querySelector('.grid')
+  //*Testing with easy level
+  const width = 9
+  const height = 9
+  const cellCount = width * height
+  const cells = []
   
+  function createGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.textContent = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+    
+  }
+
+  //*Event listeners
+  createGrid()
+
 }
 window.addEventListener('DOMContentLoaded',init)

86e0b339e55e2ad0f48c4a910c3957fe721faef5
Alberto <cerrone.alberto93@gmail.com>
2020-11-20 09:23:54 +0100

    Created a Class to produce obj with cells info

--- scripts/app.js
+++ scripts/app.js
@@ -22,18 +22,37 @@ function init() {
   const height = 9
   const cellCount = width * height
   const nBombs = 10
-  const cells = []
+  const cells = []    //maybe I can add it to the classStatusInfo
+  const cellsStatusInfo = []
+
+  //* Creating a Class to produce Object where I store info about the cell (if there is a bomb? or covered? etc)
+
+  class CellInfo{
+    constructor (idCell, isCovered, haveBomb, haveFlag, nBombsClose){
+      this.idCell = idCell           //same as i in createGrid
+      this.isCovered = isCovered            //t||f
+      this.haveBomb = haveBomb             //t||f
+      this.haveFlag = haveFlag            //t||f
+      this.nBombsClose = nBombsClose      //this will count the n of bombs around
+    }
+  }
   
+  //* Functions
+
   function createGrid() {                   //This function create the cells in the grid
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       cell.textContent = i
       grid.appendChild(cell)
-      cells.push(cell)
+      cells.push(cell)       //I should push objects with this info: hasCovered= true hasBomb:false haveflag:false bombsCloser: 0
     }
-    
   }
+console.log(cells)
+  function randomBombPosition(){
+    for (let i = 0; i < cellCount; i++){
 
+    }
+  }
 
 
   //*Event listeners
